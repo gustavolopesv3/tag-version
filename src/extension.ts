f6d86b30eeacca4ac53c371191a00642d3cbfe9b
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2023-05-30 16:29:07 -0300

    release

--- src/extension.ts
+++ src/extension.ts
@@ -3,60 +3,85 @@ import simpleGit, { SimpleGit, TagResult } from 'simple-git';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('tag-version.lastTag', async () => disposableUpdateTagVersion());
-  let disposable2 = vscode.commands.registerCommand('tag-version.getAuthorLastTag', async () => disposableGetAuthorLastTag());
 
-  context.subscriptions.push(disposable);
-  context.subscriptions.push(disposable2);
+  const disposables = [
+    {
+      name: 'tag-version.lastTag',
+      command: disposableUpdateTagVersion
+    },
+    {
+      name: 'tag-version.getAuthorLastTag',
+      command: disposableGetAuthorLastTag
+    },
+    {
+      name: 'tag-version.update-package-json',
+      command: disposableUpdateTagVersionPackageJson
+    }
+  ];
+
+  disposables.forEach(disposable => {
+    const command = vscode.commands.registerCommand(disposable.name, async () => disposable.command());
+    context.subscriptions.push(command);
+  });
+
 }
 
-function incrementVersion(tag: string): string {
-  const partes = tag.split('-');
-  if (partes.length !== 2) {
-    return tag; // Tag em formato inválido, retorna a tag original
-  }
+function disposableUpdateTagVersionPackageJson(){
+  const packageJsonPath = `${vscode.workspace.rootPath}/package.json`;
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  const version = packageJson.version;
+  const incrementedTag = _incrementVersion(version);
+  updateVersionPackageJson(packageJsonPath, incrementedTag);
+}
 
-  const prefixo = partes[0];
-  const versaoAtual = partes[1];
-  const partesVersao = versaoAtual.split('.');
-  if (partesVersao.length !== 3) {
-    return tag; // Versão em formato inválido, retorna a tag original
-  }
+function _incrementVersion(tag: string): string {
+  const partes = tag.split('.');
 
-  const major = parseInt(partesVersao[0], 10);
-  const minor = parseInt(partesVersao[1], 10);
-  const patch = parseInt(partesVersao[2], 10) + 1;
+  const major = parseInt(partes[0], 10);
+  const minor = parseInt(partes[1], 10);
+  const patch = parseInt(partes[2], 10) + 1;
 
   const novaVersao = `${major}.${minor}.${patch}`;
-  return `${prefixo}-${novaVersao}`;
+  return `${novaVersao}`;
 }
 
 
-function updateVersionPackageJson(packageJsonPath: string, novaTag: string) {
+  function updateVersionPackageJson(packageJsonPath: string, novaTag: string) {
 	try {
+    const git: SimpleGit = simpleGit(vscode.workspace.rootPath);
+
 	  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 	  packageJson.version = novaTag;
 	  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 	  vscode.window.showInformationMessage(`Tag atualizada no arquivo package.json: ${novaTag}`);
+
+    git.add(packageJsonPath);
+    git.commit(`Auto commit package.json to version ${novaTag}`);
+
+    vscode.window.showInformationMessage(`Commit realizado com sucesso no arquivo package.json!`);
+
 	} catch (error) {
 	  vscode.window.showErrorMessage(`Erro ao atualizar a tag no arquivo package.json: ${error}`);
 	}
   }
 
+
+  function _getTagVersionByPackageJson(): string {
+    const packageJsonPath = `${vscode.workspace.rootPath}/package.json`;
+    const version = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8')).version;
+    const versionNumer = version.split('-')[1] || version;
+    return `version-${versionNumer}`;
+  }
+
   async function disposableUpdateTagVersion() {
     const git: SimpleGit = simpleGit(vscode.workspace.rootPath);
     try {
-      const tags = await git.tags();
-      const latestTag = tags.latest;
-	  if(!latestTag){
-		  vscode.window.showErrorMessage('Nenhuma tag encontrada');
-		  return;
-	  }
-      const incrementedTag = incrementVersion(latestTag);
-	  await git.addTag(incrementedTag);
-	  const packageJsonPath = `${vscode.workspace.rootPath}/package.json`;
-	  updateVersionPackageJson(packageJsonPath, incrementedTag);
-      vscode.window.showInformationMessage(`Tag atual: ${latestTag}\nNova tag: ${incrementedTag}`);
+
+    const lastTagByPackageJson =  _getTagVersionByPackageJson();
+
+    await git.addTag(lastTagByPackageJson);
+
+      vscode.window.showInformationMessage(`Nova tag no git adicionada: ${lastTagByPackageJson}`);
     } catch (error) {
       vscode.window.showErrorMessage(`Erro ao obter a última tag: ${error}`);
     }
